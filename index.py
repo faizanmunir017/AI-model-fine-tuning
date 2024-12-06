c74a565e609b7cd9d6c53e5f11fc38712935b481
Faizan Munir <faizanmunir017@gmail.com>
2024-12-06 11:01:01 +0500

    Dataset read from file

--- index.py
+++ index.py
@@ -9,37 +9,21 @@ genai.configure(api_key=os.getenv("GEMINI_KEY"))
 
 print("API Key Loaded:", os.getenv("GEMINI_KEY"))
 
-training_data = [
-    {"text_input": "1", "output": "2"},
-    {"text_input": "3", "output": "4"},
-    {"text_input": "-3", "output": "-2"},
-    {"text_input": "twenty two", "output": "twenty three"},
-    {"text_input": "two hundred", "output": "two hundred one"},
-    {"text_input": "ninety nine", "output": "one hundred"},
-    {"text_input": "8", "output": "9"},
-    {"text_input": "-98", "output": "-97"},
-    {"text_input": "1,000", "output": "1,001"},
-    {"text_input": "10,100,000", "output": "10,100,001"},
-    {"text_input": "thirteen", "output": "fourteen"},
-    {"text_input": "eighty", "output": "eighty one"},
-    {"text_input": "one", "output": "two"},
-    {"text_input": "three", "output": "four"},
-    {"text_input": "seven", "output": "eight"},
-  ]
+training_data=[]
 
-# try:
-#     with open("dataset/mydata.jsonl", "r") as file:
-#         for line in file:
-#             line = line.strip()  # Strip any unnecessary whitespace
-#             if line:  # Skip empty lines
-#                 try:
-#                     training_data.append(json.loads(line))
-#                 except json.JSONDecodeError as e:
-#                     print(f"Error decoding line: {e}")
-# except FileNotFoundError:
-#     print("File not found")
+try:
+    with open("dataset/mydata.jsonl", "r") as file:
+        for line in file:
+            line = line.strip() 
+            if line:  
+                try:
+                    training_data.append(json.loads(line))
+                except json.JSONDecodeError as e:
+                    print(f"Error decoding line: {e}")
+except FileNotFoundError:
+    print("File not found")
 
-print(training_data)
+print(f"training_data: \n  {training_data}")
 
 base_model = "models/gemini-1.5-flash-001-tuning"
 
